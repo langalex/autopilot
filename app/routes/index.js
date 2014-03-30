fd04ac0d345e582b8f082221eb2dff9d40551e5f
Alexander Lang <alex@upstre.am>
2014-03-30 20:31:30 +0200

    adds build

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-export default Ember.Route.extend({
-  setupController: function(controller, model) {
-    controller.setProperties({
-      targetBearing: 0.0,
-      bearing: 0.0,
-      rudderAngle: 0.0
-    });
-  }
-});
