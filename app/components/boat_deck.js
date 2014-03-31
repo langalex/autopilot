39526f164dfc4deeb93b7f6d5d0e85a61d135e37
Alexander Lang <alex@upstre.am>
2014-03-31 11:41:18 +0200

    adds non-webkit transform

--- app/components/boat_deck.js
+++ app/components/boat_deck.js
@@ -2,9 +2,9 @@ export default Ember.Component.extend({
   classNames: ['boat'],
   bearing: 0,
   bearingCss: function() {
-    return "-webkit-transform: rotate(" + this.get('bearing') + 'deg);';
+    return '-webkit-transform: rotate(' + this.get('bearing') + 'deg); transform: rotate(' + this.get('bearing') + 'deg);';
   }.property('bearing'),
   rudderCss: function() {
-    return "-webkit-transform: rotate(" + (this.get('rudderAngle') * -1) + 'deg);';
+    return '-webkit-transform: rotate(' + (this.get('rudderAngle') * -1) + 'deg); transform: rotate(' + (this.get('rudderAngle') * -1) + 'deg)';
   }.property('rudderAngle')
 });
